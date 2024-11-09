a075372517c2aeeb996ca2b33b291af2e248ec0d
Ansh <govardhanshah456@gmail.com>
2024-11-09 16:49:49 +0530

    hi1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     eslint.configs.recommended,
@@ -11,9 +11,8 @@ export default tseslint.config(
         ignores: ["build", "node_modules", "eslint.config.mjs"],
     },
     {
-
         rules: {
-            "no-console": 'off'
+            "no-console": "off",
         },
         languageOptions: {
             parserOptions: {
