dd4413701be7b77e749de60548ba1b3cb1c77580
Ansh <govardhanshah456@gmail.com>
2024-11-10 15:59:28 +0530

    some more logging

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -6,6 +6,24 @@ const logger = winston.createLogger({
         serviceName: "auth-service",
     },
     transports: [
+        new winston.transports.File({
+            level: "info",
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            dirname: "logs",
+            filename: "app.log",
+        }),
+        new winston.transports.File({
+            level: "error",
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            dirname: "logs",
+            filename: "error.log",
+        }),
         new winston.transports.Console({
             level: "info",
             format: winston.format.combine(
