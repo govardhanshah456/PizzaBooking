d314c4ffee823a37caf875be417e826dcdbf47f3
Divye Shah <divye.shah@velotio.com>
2025-04-11 18:16:08 +0530

    all 3

--- tests/tenants/tenant.spec.ts
+++ tests/tenants/tenant.spec.ts
@@ -15,7 +15,7 @@ describe("Tenant Service", () => {
     let userToken: string;
 
     beforeAll(async () => {
-        jwks = createJWKSMock("http://0.0.0.0:5501/");
+        jwks = createJWKSMock("http://localhost:5501/");
         connection = await getTestConnection();
     });
 
